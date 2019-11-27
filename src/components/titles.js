12e84efdeb58d9fc1fe75d933d7cc8e4d936dbec
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2019-11-27 02:37:16 -0600

    proyecto libre
    proyecto libre

---
+++ src/components/titles.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1 className="title-container__title">WEATHER MAP </h1>
+                <p className="title-container__subtitle"> sergio marchena </p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
